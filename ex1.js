0f8eeb975084cf5fd8bb08f357bcc5abadd23748
LORENZZO AUSTRIQUINIO ACCIOLY <47121326809@info.local>
2024-05-07 17:11:07 -0300

    Atividades de JS

--- ex1.js
+++
@@ -1,4 +0,0 @@
-let username = prompt("Digite um nome de usuario:")
-let senha = prompt("Digite a senha: ")
-
-if ( username == "admin" &&)
--- ex3.js
+++
@@ -1,8 +0,0 @@
-const number = prompt("Digite um número: ");
-
-if(number % 2 == 0) {
-    console.log("O número é par");
-}
-else {
-    console.log("O número é ímpar");
-}
